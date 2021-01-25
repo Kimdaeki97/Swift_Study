673d44793f6c2ff2dbd2dfdffc297b885d592368
Kimdaeki97 <daeki56@gmail.com>
2021-01-25 15:38:28 +0900

    01.25

---
+++ DrawGraphics/DrawGraphics/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DrawGraphics
+//
+//  Created by 김대기 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnDrawLine(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawRectangle(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawCircle(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawArc(_ sender: UIButton) {
+    }
+    @IBAction func btnDrawFill(_ sender: UIButton) {
+    }
+    
+}
+
