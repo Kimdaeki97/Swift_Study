cd3383df5aae35bedc0404112403009ebdbbf14f
Kimdaeki97 <daeki56@gmail.com>
2021-01-26 14:56:49 +0900

    자동 레이아웃 설정하기

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by 김대기 on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
