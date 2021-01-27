813dbdb369f9bf8f29b591f09907753ae1132527
Kimdaeki97 <daeki56@gmail.com>
2021-01-27 23:18:15 +0900

    탭 터치 사용

---
+++ TapTouch/TapTouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapTouch
+//
+//  Created by 김대기 on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
