5dc2b80107f5df5376922df568b87411bb554aab
Kimdaeki97 <daeki56@gmail.com>
2021-01-28 21:33:19 +0900

    21.01.28 탭 터치 사용

--- TapTouch/TapTouch/ViewController.swift
+++ TapTouch/TapTouch/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var txtMessage: UILabel!
+    @IBOutlet var txtTapCount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
