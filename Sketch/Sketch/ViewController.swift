5ec4779c059dda7bb19a0c1a2b0be2d571ef3485
Kimdaeki97 <daeki56@gmail.com>
2021-01-29 21:31:39 +0900

    21.01.29 탭 터치

---
+++ Sketch/Sketch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sketch
+//
+//  Created by 김대기 on 2021/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
